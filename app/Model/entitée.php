1509c4a1a6430338bdd41ffef8cd0c64f49b4f3c
hamzalhadouchi <hamzabniss16@gmail.com>
2025-02-11 23:06:40 +0000

    #48 modefier la claas etape ajouter les attr cycliste document

--- app/Model/entitée.php
+++ app/Model/entitée.php
@@ -9,8 +9,10 @@ class Etape
     private string $lieuArrivee;
     private string $status;
     private string $description;
+    private array $cyclistes = [];
+    private array $document = [];
 
-    public function __construct(int $id,string $nom,float $distance,string $lieuDepart,string $lieuArrivee,string $status,string $description) {
+    public function __construct(int $id,string $nom,float $distance,string $lieuDepart,string $lieuArrivee,string $status,string $description, array $cyclistes, array $document) {
         $this->id = $id;
         $this->nom = $nom;
         $this->distance = $distance;
@@ -18,6 +20,8 @@ class Etape
         $this->lieuArrivee = $lieuArrivee;
         $this->status = $status;
         $this->description = $description;
+        $this->cyclistes = $cyclistes;
+        $this->document = $document;
     }
 
     public function __get($attr)
