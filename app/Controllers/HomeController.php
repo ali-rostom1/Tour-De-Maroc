09a56701f19b1d7c259ef0ecb1241200840dd0dd
Malika <elabiadmalika64@gmail.com>
2025-02-13 09:44:37 +0100

    Ajout de controller pour qu'il facilite le travaille il rederige directement au view  #31

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace  App\Controllers;
+
+use Core\Controller ;
+
+class HomeController extends Controller{
+    public function register(){
+        $data = ["title"=>"welcome"];
+        return $this->view("register",$data);
+    }
+
+}
